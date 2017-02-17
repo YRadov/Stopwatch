6807d302b88d21c36cc27158a0a1bcf6159edda4
YRadov <radov.yuriy@ukr.net>
2017-02-17 21:48:33 +0200

    end

--- app/src/main/java/com/yuriy/radov/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/yuriy/radov/stopwatch/StopwatchActivity.java
@@ -1,13 +1,64 @@
 package com.yuriy.radov.stopwatch;
 
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
-}
+
+    public void onClickStart(View view) {
+        running = true;
+    }//onClickStart
+
+    public void onClickStop(View view) {
+        running = false;
+    }//onClickStop
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }//onClickReset
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds / 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }//runTimer
+
+}//StopwatchActivity
